a4f36b0d0d138c928eaeeb3e590d7f9f5ab5e31f
Evening <johnchangqi@hotmail.com>
2022-10-30 15:57:22 +0800

    Expand Lit Wrapper for LR Scheduler

--- src/model/lit_wrapper.py
+++ src/model/lit_wrapper.py
@@ -1,15 +1,19 @@
 import pytorch_lightning as pl
 from torch.nn.functional import cross_entropy
 from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class LitWrapper(pl.LightningModule):
 
-    def __init__(self, model, classes, lr=0.005):
+    def __init__(self, model, classes,
+                 lr=0.005,
+                 weight_decay=0.0001):
         super(LitWrapper, self).__init__()
         self.model = model
         self.classes = classes
         self.lr = lr
+        self.weight_decay = weight_decay
 
     def training_step(self, b, b_ix):
         x, y_true = b
@@ -35,4 +39,12 @@ class LitWrapper(pl.LightningModule):
         return [self.classes[ix] for ix in y_pred.argmax(dim=1)]
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=self.lr, weight_decay=0.0001)
+        optim = Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+
+        return {
+            "optimizer": optim,
+            "lr_scheduler": {
+                "scheduler": ReduceLROnPlateau(optim, mode='max', factor=0.1, patience=2, verbose=True),
+                "monitor": "val_acc",
+            },
+        }
