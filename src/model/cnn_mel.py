373b37499a6d34324d3e6a2576915447716b55bc
Evening <johnchangqi@hotmail.com>
2022-10-30 15:58:08 +0800

    Add & Shelf CNN Mel as separate class

---
+++ src/model/cnn_mel.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+from torchaudio.transforms import MelSpectrogram
+
+
+class CNNMel(nn.Module):
+    def __init__(
+            self, n_classes: int,
+            seq_size: int = 16000,
+            mel_bins: int = 32,
+            n_fft: int = 1024
+    ):
+        super(CNNMel, self).__init__()
+
+        mel_spectrogram = MelSpectrogram(
+            sample_rate=seq_size,
+            n_fft=n_fft,
+            center=True,
+            pad_mode="reflect",
+            power=2.0,
+            normalized=True,
+            norm="slaney",
+            n_mels=mel_bins,
+            mel_scale="htk",
+        )
+
+        self.feature_extraction = nn.Sequential(
+            mel_spectrogram,
+            nn.Conv2d(1, 4, 3, 1, 1),
+            nn.BatchNorm2d(4),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(4, 4, 3, 1, 1),
+            nn.BatchNorm2d(4),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(4, 4, 3, 1, 1),
+            nn.BatchNorm2d(4),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(4, 4, 3, 1, 1),
+            nn.BatchNorm2d(4),
+            nn.ReLU(),
+        )
+        self.flatten = nn.Flatten()
+        self.classifier = nn.Sequential(
+            nn.Linear(64, n_classes),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, x):
+        x = self.feature_extraction(x)
+        x = self.flatten(x)
+        x = self.classifier(x)
+        return x
