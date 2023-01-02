304ff6e328a0fcd19a6f7f2c2ae76c8b432fbc71
Eve-ning <johnchangqi@hotmail.com>
2023-01-02 11:15:25 +0800

    Update main script

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-from pytorch_lightning import Trainer
+from matplotlib import pyplot as plt
 
 from snn_voice.datamodule.speech_command_datamodule import SpeechCommandsDataModule
 from snn_voice.model.mx.m5_cnn import M5CNN
 from snn_voice.model.mx.m5_trsafs import M5TRSAFS
+from snn_voice.utils.plot_cnn import PlotCNN
+from snn_voice.utils.plot_snn import PlotSNN
+
+# %%
 
 dm = SpeechCommandsDataModule(batch_size=16)
-# dm.prepare_data()
-# dm.setup()
-model_cnn = M5CNN()
-# model_snn = M5TRSAFS()
-#
-trainer = Trainer(
-    # fast_dev_run=True,
-    accelerator='gpu',
-    limit_train_batches=128,
-    limit_val_batches=16,
-    limit_predict_batches=4,
-    max_epochs=2
-)
-trainer.fit(model_cnn, datamodule=dm)
-preds = trainer.predict(model_cnn, datamodule=dm)
+dm.prepare_data()
+dm.setup()
+val_samples = dm.load_samples('validation')
+sample = val_samples['backward']
 # %%
-# trainer.fit(model_snn, datamodule=dm)
-# trainer.predict(model_snn, datamodule=dm)
+model_cnn = M5CNN()
+h_cnn = PlotCNN(model_cnn, )
+h_cnn.plot(sample)
+plt.show()
+# %%
+model_snn = M5TRSAFS(n_steps=15)
+# %%
+h_snn = PlotSNN(model_snn, padding_f=0.5)
+h_snn.plot(sample.repeat(15, 1, 1, 1), contrast=2,
+           resize=(50 * 15, 500))
+plt.gcf().set_figheight(13 * 4)
+plt.gcf().set_figwidth(5)
+plt.tight_layout()
+plt.show()
+# %%
