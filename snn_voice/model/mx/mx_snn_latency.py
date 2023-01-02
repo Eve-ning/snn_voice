9e3d2ca8a44b8ef45640a82148b9ae39c38b1517
Eve-ning <johnchangqi@hotmail.com>
2023-01-02 12:18:12 +0800

    Min max scale x for latency

--- snn_voice/model/mx/mx_snn_latency.py
+++ snn_voice/model/mx/mx_snn_latency.py
@@ -9,5 +9,7 @@ from snn_voice.model.mx.mx_snn import MxSNN
 class MxSNNLatency(MxSNN, ABC):
 
     def time_step_replica(self, x) -> torch.Tensor:
-        return latency(x[:, 0].abs(), num_steps=self.n_steps, clip=True,
+        x = x[:, 0].abs()
+        x = (x - x.min()) / (x.max() - x.min())
+        return latency(x, num_steps=self.n_steps, clip=True,
                        threshold=0.01, normalize=True).unsqueeze(2)
