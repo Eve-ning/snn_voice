f11333996198561c0ae647317423038d0fa71964
Eve-ning <johnchangqi@hotmail.com>
2023-01-03 16:02:51 +0800

    Add Piczak models to test

--- tests/unit_tests/test_models.py
+++ tests/unit_tests/test_models.py
@@ -1,23 +1,20 @@
 import pytest
 import pytorch_lightning as pl
-import torch
 
-from snn_voice.datamodule.sample_datamodule import SampleDataModule
-from snn_voice.datamodule.speech_command_datamodule import SpeechCommandsDataModule
-from snn_voice.model.cnn_m5 import CnnM5
-from snn_voice.model.cnn_piczak import CnnPiczak
-from snn_voice.model.lit_wrapper import LitWrapper
 from snn_voice.model.mx.m5_cnn import M5CNN
 from snn_voice.model.mx.m5_snn_latency import M5SNNLatency
 from snn_voice.model.mx.m5_snn_rate import M5SNNRate
 from snn_voice.model.mx.m5_snn_repeat import M5SNNRepeat
-from snn_voice.model.snn_tcy import SnnTCY
-from snn_voice.model.srnn_hjh import SrnnHJH
+from snn_voice.model.piczak.piczak_cnn import PiczakCNN
+from snn_voice.model.piczak.piczak_snn_latency import PiczakSNNLatency
+from snn_voice.model.piczak.piczak_snn_rate import PiczakSNNRate
+from snn_voice.model.piczak.piczak_snn_repeat import PiczakSNNRepeat
 
 
 @pytest.mark.parametrize(
     'Model',
-    [M5CNN, M5SNNRate, M5SNNRepeat, M5SNNLatency]
+    [M5CNN, M5SNNRate, M5SNNRepeat, M5SNNLatency,
+     PiczakCNN, PiczakSNNRate, PiczakSNNRepeat, PiczakSNNLatency]
 )
 def test_models(Model, dm):
     net = Model()
