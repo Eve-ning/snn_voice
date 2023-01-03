9168601a2c9fc0c72378c7f18fd76cc55d55ab21
Eve-ning <johnchangqi@hotmail.com>
2023-01-03 15:00:25 +0800

    Implement Piczak CNN

--- snn_voice/model/piczak/piczak_cnn.py
+++ snn_voice/model/piczak/piczak_cnn.py
@@ -1,21 +1,48 @@
+from abc import ABC
 from collections import OrderedDict
 
 from torch import nn
 
-from snn_voice.model.mx.mx_cnn import MxCNN
+from snn_voice.model.module.module_cnn import ModuleCNN
 from snn_voice.model.piczak.piczak_cnn_block import PiczakCNNBlock
 
 
-class PiczakCNN(MxCNN):
+class PiczakCNN(ModuleCNN, ABC):
 
-    def __init__(self, n_channel: int = 128, *args, **kwargs):
+    def __init__(self, sr: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # # We'll figure out the FFT window needed to satisfy the minimum window ms
+        # n_fft = int(MIN_WINDOW_MS / (1 / sr * 1000))
+        #
+        # self.mel_spec = MelSpectrogram(
+        #     n_mels=60,
+        #     n_fft=n_fft
+        # )
 
         self.conv_blks = nn.Sequential(
             OrderedDict([
-                ('conv_blk1', PiczakCNNBlock(1, n_channel, 57, 6, 1, 1, 4, 3, 1, 3)),
-                ('conv_blk2', PiczakCNNBlock(n_channel, n_channel, 1, 3, 1, 1, 1, 3, 1, 3)),
+                ('conv_blk1', PiczakCNNBlock(1, 80, (57, 6), (1, 1), (4, 3), (1, 3), 0.5)),
+                ('conv_blk2', PiczakCNNBlock(80, 80, (1, 3), (1, 1), (1, 3), (1, 3), None)),
             ])
         )
+        self.classifier = nn.Sequential(
+            OrderedDict([
+                ('fc1', nn.Sequential(nn.Linear(80, 5000), nn.Dropout(0.5), nn.ReLU())),
+                ('fc2', nn.Sequential(nn.Linear(5000, 5000), nn.Dropout(0.5), nn.ReLU())),
+                ('fc3', nn.Sequential(nn.Linear(5000, self.n_classes)))
+            ])
+        )
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
 
-        self.classifier = nn.Linear(n_channel, self.n_classes)
+    def forward(self, x):
+        x = self.conv_blks(x)
+        x = self.avg_pool(x).squeeze()
+        x = self.classifier(x)
+
+        return x
+
+
+# %%
+net = PiczakCNN(4000)
+net(net.example_input_array).shape
--- snn_voice/model/piczak/piczak_cnn_block.py
+++ snn_voice/model/piczak/piczak_cnn_block.py
@@ -2,32 +2,17 @@ import torch.nn as nn
 
 
 class PiczakCNNBlock(nn.Module):
-    def __init__(
-            self,
-            in_chn, out_chn,
-            ks_freq, ks_time,
-            stride_freq, stride_time,
-            pool_freq, pool_time,
-            pool_freq_stride=None, pool_time_stride=None
-    ):
-        """ This implements the M5 block in 2D """
+    def __init__(self, in_chn, out_chn, ksize, step, max_pool_ksize, max_pool_step, dropout=None):
         super().__init__()
-
-        self.conv = nn.Conv2d(
-            in_chn, out_chn,
-            kernel_size=(ks_freq, ks_time),
-            stride=(stride_freq, stride_time)
-        )
-        self.bn = nn.BatchNorm2d(out_chn)
+        self.conv = nn.Conv2d(in_chn, out_chn, ksize, step)
+        self.max_pool = nn.MaxPool2d(max_pool_ksize, max_pool_step)
+        self.dropout = nn.Dropout(dropout) if dropout else dropout
         self.relu = nn.ReLU(inplace=True)
-        self.max_pool = nn.MaxPool2d(
-            (pool_freq, pool_time),
-            (pool_freq_stride, pool_time_stride)
-        )
 
     def forward(self, x):
         x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
         x = self.max_pool(x)
+        if self.dropout:
+            x = self.dropout(x)
+        x = self.relu(x)
         return x
