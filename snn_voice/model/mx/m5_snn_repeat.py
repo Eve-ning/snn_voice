9b09a03e1decbfeb4e5884f9e29f8f998dd48784
Eve-ning <johnchangqi@hotmail.com>
2023-01-07 00:14:29 +0800

    Pull boilerplate code for both Piczak & M5 into a function

--- snn_voice/model/mx/m5_snn_repeat.py
+++ snn_voice/model/mx/m5_snn_repeat.py
@@ -1,21 +1,9 @@
-from collections import OrderedDict
-
-from torch import nn
-
 from snn_voice.model.module.module_snn_repeat import ModuleSNNRepeat
-from snn_voice.model.mx.mx_snn_block import MxSNNBlock
+from snn_voice.model.mx.m5_snn import m5_snn_init
 
 
 class M5SNNRepeat(ModuleSNNRepeat):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.conv_blks = nn.Sequential(
-            OrderedDict([
-                ('conv_blk1', MxSNNBlock(1, 128, 80, self.lif_beta, 4)),
-                ('conv_blk2', MxSNNBlock(128, 128, 3, self.lif_beta)),
-                ('conv_blk3', MxSNNBlock(128, 256, 3, self.lif_beta)),
-                ('conv_blk4', MxSNNBlock(256, 512, 3, self.lif_beta)),
-            ])
-        )
-        self.classifier = nn.Linear(512, self.n_classes)
+        m5_snn_init(self)
--- snn_voice/model/piczak/piczak_snn_latency.py
+++ snn_voice/model/piczak/piczak_snn_latency.py
@@ -1,29 +1,9 @@
-from collections import OrderedDict
-
-import torch
-from torch import nn
-
 from snn_voice.model.module.module_snn_latency import ModuleSNNLatency
-from snn_voice.model.piczak.piczak_snn_block import PiczakSNNBlock
+from snn_voice.model.piczak.piczak_snn import piczak_snn_init
 
 
 class PiczakSNNLatency(ModuleSNNLatency):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.conv_blks = nn.Sequential(
-            OrderedDict([
-                ('conv_blk1', PiczakSNNBlock(1, 80, (57, 6), (1, 1), (4, 3), (1, 3), self.lif_beta, 0.5)),
-                ('conv_blk2', PiczakSNNBlock(80, 80, (1, 3), (1, 1), (1, 3), (1, 3), self.lif_beta, 0)),
-            ])
-        )
-        self.classifier = nn.Sequential(
-            OrderedDict([
-                ('fc1', nn.Sequential(nn.Linear(80, 5000), nn.Dropout(0.5), nn.ReLU())),
-                ('fc2', nn.Sequential(nn.Linear(5000, 5000), nn.Dropout(0.5), nn.ReLU())),
-                ('fc3', nn.Sequential(nn.Linear(5000, self.n_classes)))
-            ])
-        )
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-
-        self.example_input_array = torch.rand([32, 1, 60, 101])
+        piczak_snn_init(self)
