350f984ef5ed638d6b22a07c6629bfa65aad1ad8
Eve-ning <johnchangqi@hotmail.com>
2023-01-06 23:03:13 +0800

    Adjust repeating time step replica to adapt to ndims

--- snn_voice/model/module/module_snn_repeat.py
+++ snn_voice/model/module/module_snn_repeat.py
@@ -8,4 +8,4 @@ from snn_voice.model.module.module_snn import ModuleSNN
 class ModuleSNNRepeat(ModuleSNN, ABC):
 
     def time_step_replica(self, x) -> torch.Tensor:
-        return x.repeat(self.n_steps, 1, 1, 1)
+        return x.repeat(self.n_steps, *(1,) * x.ndim)
