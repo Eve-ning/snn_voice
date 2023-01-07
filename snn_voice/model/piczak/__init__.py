5c99c9b608ae86f0096582b202f108b1043c33d3
Eve-ning <johnchangqi@hotmail.com>
2023-01-07 13:10:37 +0800

    Add __init__ __all__ to expose module to reduce import length

--- snn_voice/model/piczak/__init__.py
+++ snn_voice/model/piczak/__init__.py
@@ -1,1 +1,17 @@
-# TODO: Expose the relevant modules here to reduce import length
+from .piczak_cnn import PiczakCNN
+from .piczak_cnn_block import PiczakCNNBlock
+from .piczak_snn import piczak_snn_init
+from .piczak_snn_block import PiczakSNNBlock
+from .piczak_snn_latency import PiczakSNNLatency
+from .piczak_snn_rate import PiczakSNNRate
+from .piczak_snn_repeat import PiczakSNNRepeat
+
+__all__ = [
+    'PiczakCNN',
+    'piczak_snn_init',
+    'PiczakSNNLatency',
+    'PiczakSNNRate',
+    'PiczakSNNRepeat',
+    'PiczakCNNBlock',
+    'PiczakSNNBlock',
+]
