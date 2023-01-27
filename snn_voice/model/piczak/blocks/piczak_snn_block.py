3da1893bb423b442e549125539762e7617e7f67d
Eve-ning <johnchangqi@hotmail.com>
2023-01-27 20:47:10 +0800

    Improve Signature and add docstring

--- snn_voice/model/piczak/blocks/piczak_snn_block.py
+++ snn_voice/model/piczak/blocks/piczak_snn_block.py
@@ -5,11 +5,23 @@ from torch import nn
 class PiczakSNNBlock(nn.Module):
     def __init__(
             self,
-            in_chn, out_chn,
-            ksize, step,
-            max_pool_ksize, max_pool_step,
-            lif_beta: float, dropout=0.5
+            in_chn: int, out_chn: int,
+            ksize: int, step: int,
+            max_pool_ksize: int, max_pool_step,
+            lif_beta: float, dropout: float = 0.5
     ):
+        """ A single SNN Block to be used in Piczak Models
+
+        Args:
+            in_chn: Ingoing Channels for Conv2d
+            out_chn: Outgoing Channels for Conv2d
+            ksize: Kernel Size for Conv2d
+            step: Step Size for Conv2d
+            max_pool_ksize: Max Pool 2D Kernel Size
+            max_pool_step: Max Pool 2D Step Size
+            lif_beta: Beta for Leaky & Fire
+            dropout: Dropout probability
+        """
         super().__init__()
 
         conv = nn.Conv2d(in_chn, out_chn, ksize, step)
