a5ec877cd1a3397cb7f169a718ee58fed15521cd
Eve-ning <johnchangqi@hotmail.com>
2023-01-27 20:35:12 +0800

    Remove init_leaky from PiczakSNNBlock and simplify code

--- snn_voice/model/piczak/blocks/piczak_snn_block.py
+++ snn_voice/model/piczak/blocks/piczak_snn_block.py
@@ -1,34 +1,22 @@
 import snntorch as snn
-import torch
 from torch import nn
 
 
 class PiczakSNNBlock(nn.Module):
-    def __init__(self, in_chn, out_chn, ksize, step, max_pool_ksize, max_pool_step,
-                 lif_beta: float, dropout=0.5):
+    def __init__(
+            self,
+            in_chn, out_chn,
+            ksize, step,
+            max_pool_ksize, max_pool_step,
+            lif_beta: float, dropout=0.5
+    ):
         super().__init__()
 
-        self.conv = nn.Conv2d(in_chn, out_chn, ksize, step)
-        self.max_pool = nn.MaxPool2d(max_pool_ksize, max_pool_step)
-        self.dropout = nn.Dropout(dropout)
-        self.lif = snn.Leaky(beta=lif_beta, init_hidden=True)
-
-    def init_leaky(self) -> torch.Tensor:
-        """ Initializes the Leaky & Fire block """
-        return self.lif.init_leaky()
+        conv = nn.Conv2d(in_chn, out_chn, ksize, step)
+        max_pool = nn.MaxPool2d(max_pool_ksize, max_pool_step)
+        dropout = nn.Dropout(dropout)
+        lif = snn.Leaky(beta=lif_beta, init_hidden=True)
+        self.net = nn.Sequential(conv, max_pool, dropout, lif)
 
     def forward(self, x):
-        """ Block Forward
-
-        Args:
-            x: LIF Spikes (After Pool)
-            mem: LIF Membrane Potential. Use `init_leaky` to yield initial mem
-
-        Returns:
-            The LIF Spikes and LIF Membrane as a tuple.
-        """
-        x = self.conv(x)
-        x = self.max_pool(x)
-        x = self.dropout(x)
-        x = self.lif(x)
-        return x
+        return self.net(x)
