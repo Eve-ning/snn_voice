00bac50fd0ac5235b271987a6a41367be3f1f191
Eve-ning <johnchangqi@hotmail.com>
2023-01-29 19:36:01 +0800

    Upgrade functional init of SNNs to a nn.Module

--- snn_voice/model/mx/m5/m5_snn.py
+++ snn_voice/model/mx/m5/m5_snn.py
@@ -5,21 +5,25 @@ from torch import nn
 from snn_voice.model.mx.blocks import MxSNNBlock
 
 
-def m5_snn_init(self, lif_beta: float, n_classes: int):
-    """ Initializes the M5 __init__ blocks """
-    self.net = nn.Sequential(
-        OrderedDict([
-            ('snn',
-             nn.Sequential(
-                 MxSNNBlock(1, 128, 80, lif_beta, 4),
-                 MxSNNBlock(128, 128, 3, lif_beta),
-                 MxSNNBlock(128, 256, 3, lif_beta),
-                 MxSNNBlock(256, 512, 3, lif_beta),
-             )),
+class M5SNN(nn.Module):
+    def __init__(self, lif_beta: float, n_classes: int):
+        super().__init__()
+        self.net = nn.Sequential(
+            OrderedDict([
+                ('snn',
+                 nn.Sequential(
+                     MxSNNBlock(1, 128, 80, lif_beta, 4),
+                     MxSNNBlock(128, 128, 3, lif_beta),
+                     MxSNNBlock(128, 256, 3, lif_beta),
+                     MxSNNBlock(256, 512, 3, lif_beta),
+                 )),
 
-            ('avg_pool', nn.AdaptiveAvgPool1d(1)),
-            ('flatten', nn.Flatten(start_dim=1)),
+                ('avg_pool', nn.AdaptiveAvgPool1d(1)),
+                ('flatten', nn.Flatten(start_dim=1)),
 
-            ('fc', nn.Linear(512, n_classes))
-        ])
-    )
+                ('fc', nn.Linear(512, n_classes))
+            ])
+        )
+
+    def forward(self, x):
+        return self.net(x)
--- snn_voice/model/piczak/piczak_snn.py
+++ snn_voice/model/piczak/piczak_snn.py
@@ -5,27 +5,31 @@ from torch import nn
 from snn_voice.model.piczak.blocks import PiczakSNNBlock
 
 
-def piczak_snn_init(self, lif_beta: float, n_classes: int):
-    """ Initializes the Piczak __init__ blocks """
-    self.net = nn.Sequential(
-        OrderedDict([
-            ('snn',
-             nn.Sequential(
-                 PiczakSNNBlock(1, 80, (57, 6), (1, 1), (4, 3), (1, 3), lif_beta, 0.5),
-                 PiczakSNNBlock(80, 80, (1, 3), (1, 1), (1, 3), (1, 3), lif_beta, 0)
-             )),
+class PiczakSNN(nn.Module):
+    def __init__(self, lif_beta: float, n_classes: int):
+        super().__init__()
+        self.net = nn.Sequential(
+            OrderedDict([
+                ('snn',
+                 nn.Sequential(
+                     PiczakSNNBlock(1, 80, (57, 6), (1, 1), (4, 3), (1, 3), lif_beta, 0.5),
+                     PiczakSNNBlock(80, 80, (1, 3), (1, 1), (1, 3), (1, 3), lif_beta, 0)
+                 )),
 
-            ('avg_pool', nn.AdaptiveAvgPool2d(1)),
-            ('flatten', nn.Flatten(start_dim=1)),
+                ('avg_pool', nn.AdaptiveAvgPool2d(1)),
+                ('flatten', nn.Flatten(start_dim=1)),
 
-            ('fc',
-             nn.Sequential(
-                 nn.Sequential(nn.Linear(80, 5000), nn.Dropout(0.5), nn.ReLU()),
-                 nn.Sequential(nn.Linear(5000, 5000), nn.Dropout(0.5), nn.ReLU()),
-                 nn.Sequential(nn.Linear(5000, n_classes))
-             ),
-             )
-        ])
-    )
+                ('fc',
+                 nn.Sequential(
+                     nn.Sequential(nn.Linear(80, 5000), nn.Dropout(0.5), nn.ReLU()),
+                     nn.Sequential(nn.Linear(5000, 5000), nn.Dropout(0.5), nn.ReLU()),
+                     nn.Sequential(nn.Linear(5000, n_classes))
+                 ),
+                 )
+            ])
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
     # self.example_input_array = torch.rand([32, 1, 60, 101])
