25e53da47f453965d789d58558331bc90f1079d8
Eve-ning <johnchangqi@hotmail.com>
2023-01-29 20:48:07 +0800

    Remove removed classes

--- snn_voice/model/module/__init__.py
+++ snn_voice/model/module/__init__.py
@@ -1,15 +1,9 @@
 from .module import Module
 from .module_cnn import ModuleCNN
 from .module_snn import ModuleSNN
-from .module_snn_latency import ModuleSNNLatency
-from .module_snn_rate import ModuleSNNRate
-from .module_snn_repeat import ModuleSNNRepeat
 
 __all__ = [
     'Module',
     'ModuleCNN',
     'ModuleSNN',
-    'ModuleSNNLatency',
-    'ModuleSNNRepeat',
-    'ModuleSNNRate'
 ]
