172fd252eda36c3847ada8050cc6bc77da9bd20a
Eve-ning <johnchangqi@hotmail.com>
2023-01-29 19:55:56 +0800

    Improve model architecture naming and implement forward

--- snn_voice/model/mx/m5/m5_cnn.py
+++ snn_voice/model/mx/m5/m5_cnn.py
@@ -10,13 +10,19 @@ class M5CNN(ModuleCNN, nn.Module):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.conv_blks = nn.Sequential(
+        self.net = nn.Sequential(
             OrderedDict([
-                ('conv_blk1', MxCNNBlock(1, 128, 80, 4)),
-                ('conv_blk2', MxCNNBlock(128, 128, 3, 1)),
-                ('conv_blk3', MxCNNBlock(128, 256, 3, 1)),
-                ('conv_blk4', MxCNNBlock(256, 512, 3, 1)),
+                ('cnn',
+                 nn.Sequential(MxCNNBlock(1, 128, 80, 4),
+                               MxCNNBlock(128, 128, 3, 1),
+                               MxCNNBlock(128, 256, 3, 1),
+                               MxCNNBlock(256, 512, 3, 1))
+                 ),
+                ('avg_pool', nn.AdaptiveAvgPool1d(1)),
+                ('flatten', nn.Flatten(start_dim=1)),
+                ('fc', nn.Linear(512, self.n_classes))
             ])
         )
 
-        self.classifier = nn.Linear(512, self.n_classes)
+    def forward(self, x):
+        return self.net(x)
--- snn_voice/model/piczak/piczak_cnn.py
+++ snn_voice/model/piczak/piczak_cnn.py
@@ -1,7 +1,6 @@
 from abc import ABC
 from collections import OrderedDict
 
-import torch
 from torch import nn
 
 from snn_voice.model.module import ModuleCNN
@@ -13,26 +12,26 @@ class PiczakCNN(ModuleCNN, ABC):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.conv_blks = nn.Sequential(
+        self.net = nn.Sequential(
             OrderedDict([
-                ('conv_blk1', PiczakCNNBlock(1, 80, (57, 6), (1, 1), (4, 3), (1, 3), 0.5)),
-                ('conv_blk2', PiczakCNNBlock(80, 80, (1, 3), (1, 1), (1, 3), (1, 3), None)),
-            ])
-        )
-        self.classifier = nn.Sequential(
-            OrderedDict([
-                ('fc1', nn.Sequential(nn.Linear(80, 5000), nn.Dropout(0.5), nn.ReLU())),
-                ('fc2', nn.Sequential(nn.Linear(5000, 5000), nn.Dropout(0.5), nn.ReLU())),
-                ('fc3', nn.Sequential(nn.Linear(5000, self.n_classes)))
-            ])
-        )
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+                ('cnn',
+                 nn.Sequential(
+                     PiczakCNNBlock(1, 80, (57, 6), (1, 1), (4, 3), (1, 3), 0.5),
+                     PiczakCNNBlock(80, 80, (1, 3), (1, 1), (1, 3), (1, 3), None))
+                 ),
 
-        # self.example_input_array = torch.rand([32, 1, 60, 101])
+                ('avg_pool', nn.AdaptiveAvgPool2d(1)),
+                ('flatten', nn.Flatten(start_dim=1)),
+
+                ('fc',
+                 nn.Sequential(
+                     nn.Sequential(nn.Linear(80, 5000), nn.Dropout(0.5), nn.ReLU()),
+                     nn.Sequential(nn.Linear(5000, 5000), nn.Dropout(0.5), nn.ReLU()),
+                     nn.Sequential(nn.Linear(5000, self.n_classes))
+                 ),
+                 )
+            ])
+        )
 
     def forward(self, x):
-        x = self.conv_blks(x)
-        x = self.avg_pool(x).squeeze()
-        x = self.classifier(x)
-
-        return x
+        return self.net(x)
