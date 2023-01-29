0e884d27b0a620cad0599046140fac4007a28851
Eve-ning <johnchangqi@hotmail.com>
2023-01-29 17:46:46 +0800

    Fix missing ModuleSNN inherit

--- snn_voice/model/mx/m5/m5_snn_rate.py
+++ snn_voice/model/mx/m5/m5_snn_rate.py
@@ -1,9 +1,9 @@
-from snn_voice.model.module import ModuleSNNRate
+from snn_voice.model.module import ModuleSNNRate, ModuleSNN
 from snn_voice.model.mx.m5 import m5_snn_init
 
 
-class M5SNNRate(ModuleSNNRate):
+class M5SNNRate(ModuleSNNRate, ModuleSNN):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        m5_snn_init(self)
+        m5_snn_init(self, lif_beta=self.lif_beta, n_classes=self.n_classes)
--- snn_voice/model/mx/m5/m5_snn_repeat.py
+++ snn_voice/model/mx/m5/m5_snn_repeat.py
@@ -1,9 +1,9 @@
-from snn_voice.model.module import ModuleSNNRepeat
+from snn_voice.model.module import ModuleSNNRepeat, ModuleSNN
 from snn_voice.model.mx.m5 import m5_snn_init
 
 
-class M5SNNRepeat(ModuleSNNRepeat):
+class M5SNNRepeat(ModuleSNNRepeat, ModuleSNN):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        m5_snn_init(self)
+        m5_snn_init(self, lif_beta=self.lif_beta, n_classes=self.n_classes)
