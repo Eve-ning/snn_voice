cc9a1679d0755c7e6c96d32c48f06bd693e86aef
Eve-ning <johnchangqi@hotmail.com>
2023-01-29 19:07:04 +0800

    Restructure net to be more compact

--- snn_voice/model/mx/m5/m5_snn.py
+++ snn_voice/model/mx/m5/m5_snn.py
@@ -9,10 +9,13 @@ def m5_snn_init(self, lif_beta: float, n_classes: int):
     """ Initializes the M5 __init__ blocks """
     self.net = nn.Sequential(
         OrderedDict([
-            ('snn1', MxSNNBlock(1, 128, 80, lif_beta, 4)),
-            ('snn2', MxSNNBlock(128, 128, 3, lif_beta)),
-            ('snn3', MxSNNBlock(128, 256, 3, lif_beta)),
-            ('snn4', MxSNNBlock(256, 512, 3, lif_beta)),
+            ('snn',
+             nn.Sequential(
+                 MxSNNBlock(1, 128, 80, lif_beta, 4),
+                 MxSNNBlock(128, 128, 3, lif_beta),
+                 MxSNNBlock(128, 256, 3, lif_beta),
+                 MxSNNBlock(256, 512, 3, lif_beta),
+             )),
 
             ('avg_pool', nn.AdaptiveAvgPool1d(1)),
             ('flatten', nn.Flatten(start_dim=1)),
