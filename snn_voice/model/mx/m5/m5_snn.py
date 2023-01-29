0cec0e1aaa22ff262f062800b1687a934960795d
Eve-ning <johnchangqi@hotmail.com>
2023-01-29 17:45:44 +0800

    Add arg to init

--- snn_voice/model/mx/m5/m5_snn.py
+++ snn_voice/model/mx/m5/m5_snn.py
@@ -5,18 +5,18 @@ from torch import nn
 from snn_voice.model.mx.blocks import MxSNNBlock
 
 
-def m5_snn_init(self):
+def m5_snn_init(self, lif_beta: float, n_classes: int):
     """ Initializes the M5 __init__ blocks """
     self.net = nn.Sequential(
         OrderedDict([
-            ('snn1', MxSNNBlock(1, 128, 80, self.lif_beta, 4)),
-            ('snn2', MxSNNBlock(128, 128, 3, self.lif_beta)),
-            ('snn3', MxSNNBlock(128, 256, 3, self.lif_beta)),
-            ('snn4', MxSNNBlock(256, 512, 3, self.lif_beta)),
+            ('snn1', MxSNNBlock(1, 128, 80, lif_beta, 4)),
+            ('snn2', MxSNNBlock(128, 128, 3, lif_beta)),
+            ('snn3', MxSNNBlock(128, 256, 3, lif_beta)),
+            ('snn4', MxSNNBlock(256, 512, 3, lif_beta)),
 
             ('avg_pool', nn.AdaptiveAvgPool1d(1)),
             ('flatten', nn.Flatten(start_dim=1)),
 
-            ('fc', nn.Linear(512, self.n_classes))
+            ('fc', nn.Linear(512, n_classes))
         ])
     )
