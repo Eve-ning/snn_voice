26d7a703febe8443dd699fb04b73bb103e8f7c8d
Eve-ning <johnchangqi@hotmail.com>
2023-01-29 19:03:21 +0800

    Restructure net to be more compact

--- snn_voice/model/piczak/piczak_snn.py
+++ snn_voice/model/piczak/piczak_snn.py
@@ -9,15 +9,22 @@ def piczak_snn_init(self, lif_beta: float, n_classes: int):
     """ Initializes the Piczak __init__ blocks """
     self.net = nn.Sequential(
         OrderedDict([
-            ('snn1', PiczakSNNBlock(1, 80, (57, 6), (1, 1), (4, 3), (1, 3), lif_beta, 0.5)),
-            ('snn2', PiczakSNNBlock(80, 80, (1, 3), (1, 1), (1, 3), (1, 3), lif_beta, 0)),
+            ('snn',
+             nn.Sequential(
+                 PiczakSNNBlock(1, 80, (57, 6), (1, 1), (4, 3), (1, 3), lif_beta, 0.5),
+                 PiczakSNNBlock(80, 80, (1, 3), (1, 1), (1, 3), (1, 3), lif_beta, 0)
+             )),
 
             ('avg_pool', nn.AdaptiveAvgPool2d(1)),
             ('flatten', nn.Flatten(start_dim=1)),
 
-            ('fc1', nn.Sequential(nn.Linear(80, 5000), nn.Dropout(0.5), nn.ReLU())),
-            ('fc2', nn.Sequential(nn.Linear(5000, 5000), nn.Dropout(0.5), nn.ReLU())),
-            ('fc3', nn.Sequential(nn.Linear(5000, n_classes))),
+            ('fc',
+             nn.Sequential(
+                 nn.Sequential(nn.Linear(80, 5000), nn.Dropout(0.5), nn.ReLU()),
+                 nn.Sequential(nn.Linear(5000, 5000), nn.Dropout(0.5), nn.ReLU()),
+                 nn.Sequential(nn.Linear(5000, n_classes))
+             ),
+             )
         ])
     )
 
