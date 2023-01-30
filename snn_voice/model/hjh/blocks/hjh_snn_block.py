6ae751aade842a1cebfb453e4943ec0f7091e6b2
Eve-ning <johnchangqi@hotmail.com>
2023-01-30 22:15:49 +0800

    Update outdated SNN Block for Hjh

--- snn_voice/model/hjh/blocks/hjh_snn_block.py
+++ snn_voice/model/hjh/blocks/hjh_snn_block.py
@@ -1,34 +1,34 @@
 import snntorch as snn
-import torch
 from torch import nn
 
 
 class HjhSNNBlock(nn.Module):
-    def __init__(self, in_chn, out_chn, ksize, step, max_pool_ksize, max_pool_step,
-                 lif_beta: float, dropout=0.5):
-        super().__init__()
-
-        self.conv = nn.Conv2d(in_chn, out_chn, ksize, step)
-        self.max_pool = nn.MaxPool2d(max_pool_ksize, max_pool_step)
-        self.dropout = nn.Dropout(dropout)
-        self.lif = snn.Leaky(beta=lif_beta)
-
-    def init_leaky(self) -> torch.Tensor:
-        """ Initializes the Leaky & Fire block """
-        return self.lif.init_leaky()
-
-    def forward(self, x, mem):
-        """ Block Forward
+    def __init__(self,
+                 in_chn: int,
+                 out_chn: int,
+                 ksize: int,
+                 lif_beta: float,
+                 max_pool_ksize: int = None):
+        """ A single SNN Block to be used in Hjh Models
 
         Args:
-            x: LIF Spikes (After Pool)
-            mem: LIF Membrane Potential. Use `init_leaky` to yield initial mem
-
-        Returns:
-            The LIF Spikes and LIF Membrane as a tuple.
+            in_chn: Ingoing Channels for Conv1d
+            out_chn: Outgoing Channels for Conv1d
+            ksize: Kernel Size for Conv1d
+            max_pool_ksize: Max Pool Kernel Size, step size will be the same
+            lif_beta: Beta for Leaky & Fire
         """
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_chn, out_chn, ksize, 1, padding='same')
+        self.bn = nn.BatchNorm2d(out_chn)
+        self.max_pool = nn.MaxPool2d(max_pool_ksize, max_pool_ksize) if max_pool_ksize else None
+        self.lif = snn.Leaky(beta=lif_beta, init_hidden=True)
+
+    def forward(self, x):
         x = self.conv(x)
-        x = self.max_pool(x)
-        x = self.dropout(x)
-        x, mem = self.lif(x, mem)
-        return x, mem
+        x = self.bn(x)
+        x = self.lif(x)
+        if self.max_pool:
+            x = self.max_pool(x)
+        return x
