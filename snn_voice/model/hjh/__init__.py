b98089daec04b5908de3fdab4eeb1dde990f40d0
Eve-ning <johnchangqi@hotmail.com>
2023-01-30 21:45:24 +0800

    Update inits

--- snn_voice/model/hjh/__init__.py
+++ snn_voice/model/hjh/__init__.py
@@ -1,13 +1,7 @@
 from .hjh_cnn import HjhCNN
 from .hjh_snn import HjhSNN
-from .hjh_snn_latency import HjhSNNLatency
-from .hjh_snn_rate import HjhSNNRate
-from .hjh_snn_repeat import HjhSNNRepeat
 
 __all__ = [
     'HjhCNN',
     'HjhSNN',
-    'HjhSNNLatency',
-    'HjhSNNRate',
-    'HjhSNNRepeat',
 ]
--- snn_voice/model/piczak/__init__.py
+++ snn_voice/model/piczak/__init__.py
@@ -1,13 +1,7 @@
 from .piczak_cnn import PiczakCNN
 from .piczak_snn import PiczakSNN
-from .piczak_snn_latency import PiczakSNNLatency
-from .piczak_snn_rate import PiczakSNNRate
-from .piczak_snn_repeat import PiczakSNNRepeat
 
 __all__ = [
     'PiczakCNN',
     'PiczakSNN',
-    'PiczakSNNLatency',
-    'PiczakSNNRate',
-    'PiczakSNNRepeat',
 ]
