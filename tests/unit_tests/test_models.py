6a0f8713cb85748fd41d356c02c2ebc7e9df47ce
Eve-ning <johnchangqi@hotmail.com>
2023-01-30 10:49:22 +0800

    Make test case Models as instances due to variable args

--- tests/unit_tests/test_models.py
+++ tests/unit_tests/test_models.py
@@ -5,29 +5,29 @@ from snn_voice.model.mx.m5 import M5CNN, M5SNNLatency, M5SNNRate, M5SNNRepeat
 from snn_voice.model.piczak import PiczakCNN, PiczakSNNLatency, PiczakSNNRate, PiczakSNNRepeat
 
 
-@pytest.mark.parametrize('Model', [
-    PiczakCNN,
-    PiczakSNNRate,
-    PiczakSNNRepeat,
-    PiczakSNNLatency
+@pytest.mark.parametrize('net', [
+    PiczakCNN(),
+    PiczakSNNRate(0.5, 35, 2),
+    PiczakSNNRepeat(0.5, 35, 2),
+    PiczakSNNLatency(0.5, 35, 2),
 ])
-def test_spec_models(Model, dm_spec):
-    run_experiment(Model, dm_spec)
+def test_spec_models(net, dm_spec):
+    run_experiment(net, dm_spec)
 
 
-@pytest.mark.parametrize('Model', [
-    M5CNN,
-    M5SNNRate,
-    M5SNNRepeat,
-    M5SNNLatency
+@pytest.mark.parametrize('net', [
+    M5CNN(),
+    M5SNNRate(0.5, 35, 2),
+    M5SNNRepeat(0.5, 35, 2),
+    M5SNNLatency(0.5, 35, 2)
 ])
-def test_models(Model, dm):
-    run_experiment(Model, dm)
+def test_models(net, dm):
+    run_experiment(net, dm)
 
 
-def run_experiment(Model, dm):
-    """ Runs a singular experiment for each Model and DataModule """
-    net = Model()
+def run_experiment(net, dm):
+    """ Runs a singular experiment for each net and DataModule """
+    net = net
 
     trainer = pl.Trainer(fast_dev_run=True, accelerator='cpu')
     trainer.fit(net, datamodule=dm)
