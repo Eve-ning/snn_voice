350ac5c2a522e1ef22fe0c41c149dc23e015ed02
Eve-ning <johnchangqi@hotmail.com>
2023-01-30 21:44:27 +0800

    Make x in time_step_replica.py type explicit

--- snn_voice/utils/time_step_replica.py
+++ snn_voice/utils/time_step_replica.py
@@ -2,7 +2,7 @@ import torch
 from snntorch.spikegen import latency, rate
 
 
-def latency_replica(x, n_steps: int) -> torch.Tensor:
+def latency_replica(x: torch.Tensor, n_steps: int) -> torch.Tensor:
     x = x[:, 0].abs()
 
     # TODO: Unsure why x is unnormalized? May be of concern
@@ -11,13 +11,13 @@ def latency_replica(x, n_steps: int) -> torch.Tensor:
     return latency(x, num_steps=n_steps, clip=True, threshold=0.01, normalize=True).unsqueeze(2)
 
 
-def rate_replica(x, n_steps: int) -> torch.Tensor:
+def rate_replica(x: torch.Tensor, n_steps: int) -> torch.Tensor:
     x = x[:, 0].abs()
     x = (x - x.min()) / (x.max() - x.min())
     return rate(x, num_steps=n_steps).unsqueeze(2)
 
 
-def repeat_replica(x, n_steps: int) -> torch.Tensor:
+def repeat_replica(x: torch.Tensor, n_steps: int) -> torch.Tensor:
     # This dynamically repeats on a new axis (at the front).
     # E.g., if x.shape = (A, B, C)
     #   x.repeat(n_steps, 1, 1, 1) <- the number of 1s are dynamically yield from the shape
