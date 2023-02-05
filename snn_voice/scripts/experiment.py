96625ebc7b443fe4298eb3f7716131c7b93aaf27
Eve-ning <johnchangqi@hotmail.com>
2023-02-05 15:36:35 +0800

    Fix issue with snn args passed to nns

--- snn_voice/scripts/experiment.py
+++ snn_voice/scripts/experiment.py
@@ -1,10 +1,10 @@
 import hydra
 import pytorch_lightning as pl
-from omegaconf import OmegaConf
 from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
 
 from snn_voice.datamodule import SpeechCommandsDataModule, SampleDataModule  # noqa
 from snn_voice.model.hjh import HjhCNN, HjhSCNN, HjhSNN  # noqa
+from snn_voice.model.module import ModuleSNN
 from snn_voice.model.mx.m5 import M5CNN, M5SNN  # noqa
 from snn_voice.model.piczak import PiczakCNN, PiczakSNN  # noqa
 from snn_voice.model.tcy import TcyNN, TcySNN  # noqa
@@ -19,14 +19,18 @@ def sanitize(x: str):
 @hydra.main(version_base=None, config_path="../../conf", config_name="config")
 def experiment(cfg: ConfigSchema) -> None:
     Model = eval(sanitize((cfg_m := cfg.model).name))
-    model = Model(
-        n_classes=cfg_m.data.n_classes,
-        n_steps=cfg_m.n_steps,
-        time_step_replica=eval(sanitize(cfg_m.time_step_replica) + "_replica"),
-        learn_beta=cfg_m.learn_beta,
-        learn_thres=cfg_m.learn_thres,
-        beta=cfg_m.beta,
-    )
+
+    if issubclass(Model, ModuleSNN):
+        model = Model(
+            n_classes=cfg_m.data.n_classes,
+            n_steps=cfg_m.snn.n_steps,
+            time_step_replica=eval(sanitize(cfg_m.snn.time_step_replica) + "_replica"),
+            learn_beta=cfg_m.snn.learn_beta,
+            learn_thres=cfg_m.snn.learn_thres,
+            beta=cfg_m.snn.beta,
+        )
+    else:
+        model = Model(n_classes=cfg_m.data.n_classes)
     DataModule = eval(sanitize((cfg_d := cfg_m.data).name))
     dm = DataModule(
         n_mels=cfg_d.n_mels,
